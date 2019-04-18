a7a01ecce59b2f9f83a934020313f77267db2fcf
hoffm386 <erinrhof@uw.edu>
2019-04-18 16:40:11 -0700

    Updates for April 2019
    1. Made the models inherit from ApplicationRecord
    2. Specified the migration versions and reduced the number of migrations from 3 to 2
    3. Removed template author spec because there aren't any actual tests for Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: authors
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
-class Author < ActiveRecord::Base
+class Author < ApplicationRecord
   has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119185429) do
+ActiveRecord::Schema.define(version: 20190418213203) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +21,10 @@ ActiveRecord::Schema.define(version: 20160119185429) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "description"
+    t.string   "description"
+    t.integer  "author_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "author_id"
   end
 
 end
